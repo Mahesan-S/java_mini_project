c1b16214d4168a31c1f5e68efd9fa4ab144a5ac8
Mahesan S <137515993+Mahesan-S@users.noreply.github.com>
2023-08-23 20:41:34 +0530

    Update Showroom.java

--- showroom/Showroom.java
+++ showroom/Showroom.java
@@ -32,92 +32,91 @@ public class Showroom {
 					System.out.println("----------------------------------------------------");
 					int opt = scan.nextInt();
 //case 1 -----------------------------------------------------------------------
-									switch(opt) {
-									case 1:
+					switch(opt) {
+					case 1:
+						
+						System.out.println("these our mobile collection");
+						System.out.println("*****************************************************");
+						for(Mobile m : mobiles) {
+							m.displaydetail();
+							}
+						
+						System.out.println("*****************************************************");
+						//ArrayList<Mobile> mobile = mobiles;
+						System.out.println("which mobile sir :-");
 										
-										System.out.println("these our mobile collection");
-										System.out.println("*****************************************************");
-										for(Mobile m : mobiles) {
-											m.displaydetail();
+						Customer customer = new Customer();
+						
+						if(customer.isavailable(mobiles,customers)) {
+							customers.add(customer);
+							
+							System.out.println("yes sir we have this mobile");
+							System.out.println("-------------------------------------");
+							System.out.println("we have to bill the mobile press ( 1 )");
+							
+							System.out.println("fill the customer detail");
+							customer.customerdetail();
+							System.out.println("--------------------------------------");
+							System.out.println("u r details");
+								
+							customer.customername();
+							System.out.println("----------------------------------------");
+													
+							System.out.println("EMI ( 1 ) or CASH ( 2 )");
+							int ec = scan.nextInt();
+							switch(ec) {
+							
+								
+							case 1:
+								
+								customer.emidetail(customers);
+								customer.emicalculation(mobiles,customers);
+									
+								if(customer.ispaidemi(customers)) {
+									
+									System.out.println("customer EMI paid :- " + customer.getemipaid());
+									System.out.println("u r emi amount is :- " + customer.getemi());
+									}
+									customer.emibill();
+									break;
+														
+								case 2:
+									
+									if(customer.ispaid(customers)) {
+										
+										//customer.customername();
+										System.out.println("paid " + customer.getpaid());
+										System.out.println("-----------------------------");
+										customer.bill();
 										}
-										System.out.println("*****************************************************");
-										
-//										ArrayList<Mobile> mobile = mobiles;
-										System.out.println("which mobile sir :-");
-										
-										
-										Customer customer = new Customer();
-											
-											if(customer.isavailable(mobiles,customers)) {
-												customers.add(customer);
-												
-												System.out.println("yes sir we have this mobile");
-												System.out.println("-------------------------------------");
-												System.out.println("we have to bill the mobile press ( 1 )");
-												
-												int cus = scan.nextInt();
-												
-												if(cus == 1) {
-													System.out.println("fill the customer detail");
-													customer.customerdetail();
-													System.out.println("--------------------------------------");
-													System.out.println("u r details");
-													customer.customername();
-													System.out.println("----------------------------------------");
-													
-													System.out.println("EMI ( 1 ) or CASH ( 2 )");
-													int ec = scan.nextInt();
-													switch(ec) {
-													
-													case 1:
-														
-														customer.emidetail(customers);
-														customer.emicalculation(mobiles,customers);
-														if(customer.ispaidemi(customers)) {
-															System.out.println("customer EMI paid :- " + customer.getemipaid());
-															System.out.println("u r emi amount is :- " + customer.getemi());
-														}
-														customer.emibill();
-														break;
-														
-													case 2:
-														
-														if(customer.ispaid(customers)) {
-//															customer.customername();
-															System.out.println("paid " + customer.getpaid());
-															System.out.println("-----------------------------");
-															customer.bill();
-														}
-														break;
-													default :
-														System.out.println("u r enter the wrong option ");
-													}
-													
-												}
-												else {
-													System.out.println("i am visiting again");
-												}
-												
-											}
-											else {
-												System.out.println("sorry dont't have this mobile");
-											}
 										break;
+										
+								default :
+									System.out.println("u r enter the wrong option ");
+									
+								}
+												
+								 }
+								else {
+									System.out.println("sorry dont't have this mobile");
+									}
+									break;
 //-------------------------------------------------------------------------------------					
-									case 2:
-										System.out.println("these our mobile collection");
-										System.out.println("*****************************************************");
-										for(Mobile m : mobiles) {
-											m.displaydetail();
+								case 2:
+									System.out.println("these our mobile collection");
+									System.out.println("*****************************************************");
+									for(Mobile m : mobiles) {
+										m.displaydetail();
 										}
-										System.out.println("if u want to buy press ( 1 ) exit press ( 0 )");
-										loopcase = scan.nextInt();
-										if(loopcase == 1)
-											System.out.println("conform press ( 1 ) exit press ( 0 ) ");
-										else {
+									
+									System.out.println("if u want to buy press ( 1 ) exit press ( 0 )");
+									loopcase = scan.nextInt();
+									if(loopcase == 1)
+										System.out.println("conform press ( 1 ) exit press ( 0 ) ");
+										
+									else {
 											System.out.println("thanks vistin again");
 											loopcase = 0;
-											
 										}
 										break;
 										
